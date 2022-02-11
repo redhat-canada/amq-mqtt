022424ed48b4b1d447804a9cb2e005834494cf6c
Andrew Pitt <apitt@redhat.com>
2022-02-11 13:24:28 -0500

    Uncommented lines in cert script.

--- cert.sh
+++ cert.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Get cluster apps domain
-# appsurl=$(oc get ingresses.config.openshift.io cluster  -o template --template '{{.spec.domain}}')
-# brokerurl="amq-mqtt-mqtt-0-svc-rte-amq-mqtt.$appsurl"
-# echo "Broker URL: $brokerurl"
+appsurl=$(oc get ingresses.config.openshift.io cluster  -o template --template '{{.spec.domain}}')
+brokerurl="amq-mqtt-mqtt-0-svc-rte-amq-mqtt.$appsurl"
+echo "Broker URL: $brokerurl"
 
-# keytool -genkey -alias broker -keyalg RSA -keystore broker.ks -storepass $1 -dname CN=$brokerurl
-# keytool -export -alias broker -keystore broker.ks -file broker_cert -storepass $1 
-# keytool -genkey -alias client -keyalg RSA -keystore client.ks -storepass $1 -dname CN=localhost
-# keytool -import -alias broker -keystore client.ts -file broker_cert -storepass $1 -noprompt
+keytool -genkey -alias broker -keyalg RSA -keystore broker.ks -storepass $1 -dname CN=$brokerurl
+keytool -export -alias broker -keystore broker.ks -file broker_cert -storepass $1 
+keytool -genkey -alias client -keyalg RSA -keystore client.ks -storepass $1 -dname CN=localhost
+keytool -import -alias broker -keystore client.ts -file broker_cert -storepass $1 -noprompt
 oc create secret generic amq-mqtt-mqtt-secret \
     --from-file=broker.ks=broker.ks \
     --from-file=client.ts=broker.ks \
